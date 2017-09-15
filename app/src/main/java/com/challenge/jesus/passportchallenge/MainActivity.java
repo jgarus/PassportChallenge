4df0574d05164213690313a623df6b2ac6f5ac0c
jesus <javprg7@gmail.com>
2017-09-14 20:26:38 -0400

    initial commit

---
+++ app/src/main/java/com/challenge/jesus/passportchallenge/MainActivity.java
@@ -0,0 +1,8 @@
+package com.challenge.jesus.passportchallenge;
+
+/**
+ * Created by jesus on 9/14/17.
+ */
+
+public class MainActivity {
+}
---
+++ app/src/main/java/com/challenge/jesus/passportchallenge/User.java
@@ -0,0 +1,14 @@
+package com.challenge.jesus.passportchallenge;
+
+/**
+ * Created by jesus on 9/14/17.
+ */
+
+public class User {
+
+    //What we need Id, background color, gender, name, age, profile image, hobbies
+
+    private String background_color, gender, name, image, hobies;
+    private int _id, age;
+
+}

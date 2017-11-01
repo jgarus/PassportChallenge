fbd200cdf4a50c05b8da10047db6f6dedf968260
jgarus <javprg7@gmail.com>
2017-11-01 13:15:36 -0400

    Added edit button, added add/edit user layout

--- app/src/main/java/com/challenge/jesus/passportchallenge/DisplayUserProfileActivity.java
+++ app/src/main/java/com/challenge/jesus/passportchallenge/DisplayUserProfileActivity.java
@@ -1,11 +1,15 @@
 package com.challenge.jesus.passportchallenge;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -58,5 +62,33 @@ public class DisplayUserProfileActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit_user, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_edit:
+                editUser();
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void editUser() {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, new AddUserFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }

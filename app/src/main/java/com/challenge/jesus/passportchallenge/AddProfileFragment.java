afc87cff2922be692a822dbc336630c6b6d21473
jesus <javprg7@gmail.com>
2018-04-01 18:06:27 -0400

    Layout fixes

--- app/src/main/java/com/challenge/jesus/passportchallenge/AddProfileFragment.java
+++ app/src/main/java/com/challenge/jesus/passportchallenge/AddProfileFragment.java
@@ -8,6 +8,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
@@ -67,6 +68,10 @@ public class AddProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_user, container, false);
+
+        // Disable collapsing title
+        CollapsingToolbarLayout collapsingToolbarLayout = view.findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitleEnabled(false);
 
         genderSelectionSpinner = view.findViewById(R.id.spinner);
 
